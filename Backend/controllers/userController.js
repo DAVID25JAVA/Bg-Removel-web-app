dbc7d4e8c57d438ef8163cf6963826267aed25fc
David <dp2072000@gmail.com>
2025-11-25 01:21:19 +0530

    push

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -17,20 +17,22 @@ const clerkWebhook = async (req, res) => {
       case "user.created": {
         const userData = {
           clerkId: data?.id,
-          email: data?.email_address[0].email_address,
+          email: data?.email_addresses[0].email_address,
           firstName: data?.first_name,
           lastName: data?.last_name,
           photo: data?.image_url,
         };
 
         await User.create(userData);
+        console.log(userData);
+        
         res.json({});
 
         break;
       }
       case "user.updated": {
         const userData = {
-          email: data?.email_address[0].email_address,
+          email: data?.email_addresses[0].email_address,
           firstName: data?.first_name,
           lastName: data?.last_name,
           photo: data?.image_url,
