dddf0c0e5d73dbab70f827c0950d8b028687cd8f
David <dp2072000@gmail.com>
2025-11-25 00:48:39 +0530

    push code

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,58 @@
+import { Webhook } from "svix";
+import User from "../models/userModel.js";
+
+const clerkWebhook = async (req, res) => {
+  try {
+    const whook = new Webhook(process?.env?.CLERK_WEBHOOK_SECRET);
+    console.log(whook);
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamps": req.headers["svix-timestamps"],
+      "svix-signature": req.headers["svix-signature"],
+    });
+
+    const { data, type } = req?.body;
+
+    switch (type) {
+      case "user.created": {
+        const userData = {
+          clerkId: data?.id,
+          email: data?.email_address[0].email_address,
+          firstName: data?.first_name,
+          lastName: data?.last_name,
+          photo: data?.image_url,
+        };
+
+        await User.create(userData);
+        res.json({});
+
+        break;
+      }
+      case "user.updated": {
+        const userData = {
+          email: data?.email_address[0].email_address,
+          firstName: data?.first_name,
+          lastName: data?.last_name,
+          photo: data?.image_url,
+        };
+
+        await User.findOneAndUpdate({ clerkId: data?.id }, userData);
+            res.json({});
+        break;
+      }
+        case "user.deleted": {
+            await User?.findOneAndDelete({ clerkId: data?.id });
+            res?.json({})
+        break;
+      }
+
+      default:
+        break;
+    }
+  } catch (error) {
+    console.log("web hook error:", error?.message);
+    res.json({ success: false, message: "something went wrong" });
+  }
+};
+
+export {clerkWebhook}
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 await connectDB();
 
@@ -10,7 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// user routes
+app.use("/api/user", userRouter)
+
 app.get("/", (req, res) => res.send("API working fine........."));
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
